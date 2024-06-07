5c37cca518117b8ef842aa125e63fabe17e7661e
Dream-Work <marnitz.sven@gmail.com>
2024-06-07 13:29:02 +0200

    CHANGE: update eslint config (#214)

---
+++ eslint.config.js
@@ -0,0 +1,29 @@
+// @ts-check
+
+import eslint from '@eslint/js'
+import prettierConfig from 'eslint-config-prettier'
+import tseslint from 'typescript-eslint'
+
+export default tseslint.config(
+  eslint.configs.recommended,
+  ...tseslint.configs.recommendedTypeChecked,
+  prettierConfig,
+  {
+    languageOptions: {
+      parserOptions: {
+        project: true,
+        tsconfigRootDir: import.meta.dirname
+      }
+    }
+  },
+  {
+    rules: {
+      '@typescript-eslint/no-namespace': 'off',
+      '@typescript-eslint/no-explicit-any': 'off',
+      '@typescript-eslint/no-redundant-type-constituents': 'off'
+    }
+  },
+  {
+    ignores: ['test-results', 'playwright-report']
+  }
+)
